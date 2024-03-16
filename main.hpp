bffbb4ce2f759a5fc57226630572389af126d36a
khanatal92 <akhan126@insite.4cd.edu>
2024-03-15 19:14:42 -0700

    commit

--- main.hpp
+++ main.hpp
@@ -1,8 +1,18 @@
+#include <iostream>
+#include <cmath>
 
-//********************
-// Declare Function Prototypes
-//********************
+using namespace std;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+// Function prototypes
+void getTwoValues(int &begin, int &end);
+bool isPrime(int num);
+int getNextPrime(int begin);
+int getPrevPrime(int end);
+
+// Function to get two integer values from the user
+void getTwoValues(int &begin, int &end) {
+    do {
+        cout << "Enter two integer values (begin < end): ";
+        cin >> begin >> end;
+    } while (begin >= end);
+}
